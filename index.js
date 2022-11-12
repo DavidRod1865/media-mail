4e2967b5b64f121334a08cd11d587f29db597722
David Rodriguez <David.Rod1865@gmail.com>
2022-11-11 22:53:55 -0500

    Fixed MIME Error

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const cors = require('cors')
+const path = require('path')
 
-app.use(express.static('/public'))
+app.use(express.static(path.join(__dirname, "public")));
 
 //Send Index.html to Server
 app.get('/', (req, res) => {
